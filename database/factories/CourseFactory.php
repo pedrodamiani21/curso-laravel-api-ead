819ff5cac23228b58211b23a0b65823eee6b0271
Pedro Damiani <pedrodamiani21@gmail.com>
2022-01-17 16:34:29 -0300

    create api cursos

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Course;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CourseFactory extends Factory
+{
+    protected $model = Course::class;
+
+    public function definition()
+    {
+        return [
+             'name'=> $this->faker->unique()->name(),
+             'description'=> $this->faker->sentence(20),
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\{
+    CourseController
+};
 use Illuminate\Support\Facades\Route;
+
+Route::get('/courses', [CourseController::class, 'index']);
+Route::get('/courses/{id}', [CourseController::class, 'show']);
 
 Route::get('/', function(){
     return response()->json([
